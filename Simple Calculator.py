d11fd3d829a67203a969fee4bb1e956c54f34a80
unknown <rv8828120@gmail.com>
2023-05-09 22:12:51 +0800

    Pseudocode

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,3 +1,11 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+#   Prompting the user to select an operation
+#   Obtain user input for the operation
+#   Collect two numbers from the user
+#   Execute an operation based on user input
+#   Print the result
+#   Request if the user wants to make another computation.
+#   If the user does not want to execute further calculation, the function should be closed.
