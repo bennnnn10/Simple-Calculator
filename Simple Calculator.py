db755cf3ded625da458cbbabad82cadc9bb03ab6
unknown <rv8828120@gmail.com>
2023-05-09 22:39:14 +0800

    Collect two numbers from the user

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -9,9 +9,11 @@ def calculator():
         print("Addition(1), Subtraction(2), Multiplication(3), or Division(4)")
         #   Obtain user input for the operation
         math_operation = int(input("Choose one of the four math operations (1-4): "))
-#   Collect two numbers from the user
-#   Execute an operation based on user input
-#   Print the result
+        #   Collect two numbers from the user
+        first_number = input("Enter the first number: ")
+        second_number = input("Enter the second number: ")
+        #   Execute an operation based on user input
+        #   Print the result
     #   Handle runtime errors
     #   Handle division by zero error
     except ZeroDivisionError:
