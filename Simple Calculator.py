bb4a193a45abe70b153192e7064cc5bdb391a5ab
unknown <rv8828120@gmail.com>
2023-05-10 01:15:12 +0800

    Fixing errors and adding designs

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -21,17 +21,20 @@ def calculator():
         #   Prompting the user to select an operation
         print("✹" * 78)
         print("\n")
-        print("Please choose an operation")
-        print("Addition(1), Subtraction(2), Multiplication(3), or Division(4)")
+        print(colored("Please choose an operation\n".center(75), "yellow"))
+        print(colored("Addition(1), Subtraction(2), Multiplication(3), or Division(4)\n".center(75), "yellow"))
 
         #   Obtain user input for the operation
-        math_operation = int(input("Choose one of the four math operations (1-4): "))
+        print(colored("Choose one of the four math operations (1-4): ".center(75), "cyan"))
+        math_operation = int(input("".center(37)))
+        print("\n")
 
         #   Collect two numbers from the user
-        first_number = float(input("Enter the first number: "))
-        second_number = float(input("Enter the second number: "))
+        first_number = float(input("\033[;1;3mEnter the first number: \033[0m"))
+        second_number = float(input("\033[;1;3mEnter the second number: \033[0m"))
 
         #   Execute an operation based on user input
+        result = None  # Initialize result with a default value
         if math_operation == 1:
             result = first_number + second_number
         elif math_operation == 2:
@@ -42,29 +45,38 @@ def calculator():
             result = first_number / second_number
         else:
             #   Deal with an invalid operation number
-            print("Invalid operation number.")
+            print()
+            print(colored("Invalid operation number.\n".center(75), "red"))
             calculator()
 
         #   Print the result
-        print("The result is:", result)
+        print()
+        print("\033[;1;92;3m➺   The result is", result, "\033[0m")
 
         #   Request if the user wants to make another computation.
-        try_again = input("Would you like to make another calculation? (y/n): ")
+        print("\n")
+        try_again = input(colored("Would you like to make another calculation? (y/n): ", "yellow"))
+        print("\n")
 
         if try_again.lower() == "y":
             calculator()
         else:
         #   If the user does not want to execute further calculation, the function should be closed.
-            print("Thank you")
+            print("✹" * 78)
+            print("\n")
+            print(pyfiglet.figlet_format("Thank you!".center(11), justify="center"))
+            exit()
 
     #   Handle runtime errors
     #   Handle division by zero error
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        print()
+        print(colored("Error: Cannot divide by zero.\n".center(75), "red"))
         calculator()
     #   Handle invalid input error
     except ValueError:
-        print("Error: Invalid input.")
+        print()    
+        print(colored("Error: Invalid input.\n".center(75), "red"))
         calculator()
 
 calculator()
